888dc6ed4fd2e86a97470cfefe47c6d0ccce85dd
chao_w <chao_w@ctrip.com>
2019-01-14 21:57:18 +0800

    尝试git多账号运作

---
+++ src/main/java/chaoking/java/allinone/AllinoneApplication.java
@@ -0,0 +1,14 @@
+package chaoking.java.allinone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllinoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllinoneApplication.class, args);
+	}
+
+}
+
