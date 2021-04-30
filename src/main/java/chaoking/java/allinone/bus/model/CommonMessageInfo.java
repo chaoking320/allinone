1d9da468ae4415de43473eddd7f28b611aa0b61a
chao_w <chao_w@trip.com>
2021-04-30 15:18:34 +0800

    bus 监听器，可以识别 “类型” 进行监听

---
+++ src/main/java/chaoking/java/allinone/bus/model/CommonMessageInfo.java
@@ -0,0 +1,22 @@
+package chaoking.java.allinone.bus.model;
+
+public class CommonMessageInfo {
+    private String code;
+    private String name;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
