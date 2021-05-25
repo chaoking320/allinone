d99be709595077e9bb5c8ca0b4a9c772dbe62517
chao_w <chao_w@trip.com>
2021-05-25 17:50:31 +0800

    Executor 测试

---
+++ src/main/java/chaoking/java/allinone/learn/thread/executortest/ExecutorTest.java
@@ -0,0 +1,45 @@
+package chaoking.java.allinone.learn.thread.executortest;
+
+import chaoking.java.allinone.learn.thread.NamedThreadFactory;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorTest implements Runnable {
+
+    private final Executor executor;
+    private final Executor executor2;
+
+    public ExecutorTest(Executor executor,Executor executor2) {
+        this.executor = executor;
+        this.executor2 = executor2;
+    }
+
+    @Override
+    public void run() {
+        Thread thread = Thread.currentThread();
+        String threadName = thread.getName();
+        int a = 1+1;
+        int b = 2+a;
+        System.out.println(b);
+        System.out.println("进入 ExecutorTest 中的 run 方法"+ threadName);
+    }
+
+    public void start(){
+        executor.execute(this);
+    }
+
+    public void start2(){
+        executor2.execute(this);
+    }
+
+
+    public static void main(String[] args) {
+        ExecutorService pullExecutor = Executors.newCachedThreadPool(new NamedThreadFactory("executortest"));
+        ExecutorService pullExecutor2 = Executors.newCachedThreadPool(new NamedThreadFactory("executortest2"));
+        ExecutorTest test = new ExecutorTest(pullExecutor,pullExecutor2);
+        test.start();
+        test.start2();
+    }
+}
