889fa7ede19739f8fc41aff435f5f56d69c92040
chao_w <chao_w@trip.com>
2021-06-28 10:59:13 +0800

    threadlocal 2 线程池+大对象 可能有点问题

--- src/main/java/chaoking/java/allinone/learn/thread/MyThreadLocal.java
+++ src/main/java/chaoking/java/allinone/learn/thread/MyThreadLocal.java
@@ -1,5 +1,16 @@
 package chaoking.java.allinone.learn.thread;
 
+import sun.nio.ch.ThreadPool;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
+import java.lang.management.OperatingSystemMXBean;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 /**
  * 意在说明ThreadLocal的使用场景及方法：即多个线程使用同一个对象的时候为了避免冲突，线程将对象在本地赋值，这样就做到了互不干扰
  * 但是在线程池的场景中，如果不主动remove，则可能造成内存泄露
@@ -10,6 +21,7 @@ public class MyThreadLocal {
 
         Person person =new Person();
 
+        // region 简单演示ThreadLocal
         new Thread(new Runnable() {
             @Override public void run() {
                 person.setName("Hello");
@@ -38,10 +50,34 @@ public class MyThreadLocal {
             }
         }).start();
 
+        //  endregion
+
+        ThreadPoolExecutor executor =
+            new ThreadPoolExecutor(10, 20, 0L, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(5), Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
+
+        BigClass big = new BigClass();
+
+        OperatingSystemMXBean osmxb = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+        MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
+        // 椎内存使用情况
+        MemoryUsage memoryUsage = memoryMXBean.getHeapMemoryUsage();
+
+        int index = 0;
+        for (Integer i=0;i<25;i++){
+            index++;
+            executor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    person.setBigClass(big);
+                    System.out.println("当前内存占用："+memoryMXBean.getHeapMemoryUsage().getUsed());
+                }
+            });
+        }
     }
 
     static class Person{
         private ThreadLocal<String> name = new ThreadLocal<>();
+        private ThreadLocal<BigClass> bigClass = new ThreadLocal<>();
 
         public void setName(String name){
             this.name.set(name);
@@ -56,6 +92,22 @@ public class MyThreadLocal {
          */
         public void remove(){
             this.name.remove();
+            this.bigClass.remove();
+        }
+
+        public void setBigClass(BigClass big) {
+            this.bigClass.set(big);
+        }
+
+        public BigClass getBigClass(){
+            return this.bigClass.get();
+        }
+    }
+
+    static class BigClass{
+        private String[] arrays;
+        public BigClass() {
+            arrays = new String[1024 * 1024 * 1024];
         }
     }
 }
