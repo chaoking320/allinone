53f140278fbae0cfde24873820f793c04b8aa34b
chao_w <chao_w@trip.com>
2024-05-11 15:28:12 +0800

    vi init

--- vi/src/main/java/allinone/vi/ViApplication.java
+++ vi/src/main/java/allinone/vi/ViApplication.java
@@ -2,12 +2,20 @@ package allinone.vi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+import java.awt.*;
+import java.net.URI;
 
 @SpringBootApplication
-public class ViApplication {
+@ServletComponentScan
+public class ViApplication  {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         SpringApplication.run(ViApplication.class, args);
+
+        System.setProperty("java.awt.headless", "false");
+        Desktop.getDesktop().browse(new URI("http://127.0.0.1:9090/@in"));
     }
 
 }
---
+++ vi/src/main/java/allinone/vi/filterchain/MyInterceptor.java
@@ -0,0 +1,30 @@
+package allinone.vi.filterchain;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by chao_w on 2024/5/10.
+ */
+@Component
+public class MyInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        System.out.printf("interceptor 前置");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+        System.out.printf("interceptor 处理中");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+        System.out.printf("interceptor 后置");
+    }
+}
