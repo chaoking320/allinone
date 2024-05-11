d4ee7204ef607088fff753df2214ed371b97078e
chao_w <chao_w@trip.com>
2024-05-11 14:05:38 +0800

    vi init

---
+++ vi/src/main/java/allinone/vi/ViApplication.java
@@ -0,0 +1,13 @@
+package allinone.vi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViApplication.class, args);
+    }
+
+}
